482a4be79331d461a660f53ee0dae4c5ee15630f
tripack45 <patrickyao@sjtu.edu.cn>
2016-12-07 02:48:21 +0800

    Feat. Refactor TruthTable for names

--- common/truth_table.cpp
+++ common/truth_table.cpp
@@ -5,6 +5,7 @@
 #include "truth_table.h"
 #include "exception.h"
 #include <iostream>
+#include <cassert>
 
 using namespace std;
 
@@ -12,20 +13,20 @@ TruthTable::TruthTable(int inputSize) {
     if(inputSize < 0)
         throw ExceptionTruthTableInvalidSize();
     this->inputSize = inputSize;
+    for (int i = 0; i < inputSize; ++i) {
+        name.push_back(std::string("in") + std::to_string(i));
+    }
 }
 
-TruthTable::TruthTable(int inputSize, std::vector<int>&& v) {
-    if(inputSize < 0)
-        throw ExceptionTruthTableInvalidSize();
-    this->inputSize = inputSize;
-    setByVector(v);
-}
 
 TruthTable::TruthTable(int inputSize, std::vector<int> &v) {
     if(inputSize < 0)
         throw ExceptionTruthTableInvalidSize();
     this->inputSize = inputSize;
-    setByVector(std::move(v));
+    setByVector(v);
+    for (int i = 0; i < inputSize; ++i) {
+        name.push_back(std::string("in") + std::to_string(i));
+    }
 }
 
 const BoolVariable &TruthTable::operator[](int i) const {
@@ -36,17 +37,37 @@ BoolVariable &TruthTable::operator[](int i) {
     return data.at(i);
 }
 
-void TruthTable::setByVector(std::vector<int>&& v) {
-    int size = v.size();
-    int nInput = this->inputSize;
-    if (1 << nInput != size) {
+void TruthTable::setByVector(std::vector<int>& v) {
+    if (this->size() != v.size()) {
         cerr << "TruthTable: "
                 "Error during vector assignment, "
                 "size mismatch." << endl;
         throw ExceptionInvalidVectorAssignment{};
     }
-    for (int i = 0; i < size; ++i) {
-        this->data[i] = v[i];
+    this->data.clear();
+    for (int i = 0; i < size(); ++i) {
+        this->data.push_back(BoolVariable(v[i]));
     }
     return;
+}
+
+void TruthTable::setNameByVector(std::vector<std::string> &name) {
+    for (int i = 0; i < name.size(); ++i) {
+        this->name[i] = name[i];
+        if (i == inputSize) break;
+    }
+}
+
+std::string &TruthTable::getName(int i) {
+    if (i > inputSize) assert(0);
+    return this->name[i];
+}
+
+void TruthTable::print() {
+    cout << "Inputs: " << endl;
+    for (auto& n : this->name)
+        cout << n << "\t";
+    cout << endl << "Table: " << endl;
+    for (auto& i : this->data)
+        cout << (int)i << " ";
 }
--- common/truth_table.h
+++ common/truth_table.h
@@ -10,54 +10,54 @@
 
 class BoolVariable {
 public:
-    BoolVariable() : state(FALSE) {};
+    BoolVariable() : state(BoolFALSE) {};
     BoolVariable(const BoolVariable& b) = default;
-    BoolVariable(const BoolVariable&& b) = default;
+    BoolVariable(BoolVariable&& b) = default;
     explicit BoolVariable(const bool& b) { copy(b); };
     explicit BoolVariable(const int& i) { copy(i); }
 
     BoolVariable& operator=(const BoolVariable& b) = default;
-    BoolVariable& operator=(const BoolVariable&& b) = default;
-    explicit BoolVariable& operator=(const bool& b) {
+    BoolVariable& operator=(BoolVariable&& b) = default;
+    BoolVariable& operator=(const bool& b) {
         return copy(b);
     }
-    explicit BoolVariable& operator=(const int& i) {
+    BoolVariable& operator=(const int& i) {
         return copy(i);
     }
 
     explicit operator bool() const {
-        if (state == TRUE) return true;
+        if (state == BoolTRUE) return true;
     }
     explicit operator int() const {
-        if (state == TRUE) return 1;
+        if (state == BoolTRUE) return 1;
         if (state == DC) return -1;
         return 0;
     }
 
     explicit operator const char*() const {
-        if (state == TRUE) return "true";
+        if (state == BoolTRUE) return "true";
         if (state == DC) return "dont_care";
         return "false";
     }
 
     std::string toString() const {
-        if (state == TRUE) return "true";
+        if (state == BoolTRUE) return "true";
         if (state == DC) return "dont_care";
         return "false";
     }
 
 private:
     BoolVariable& copy(const bool& b) {
-        if (b) {state = TRUE; return *this;}
-        state = FALSE;
+        if (b) {state = BoolTRUE; return *this;}
+        state = BoolFALSE;
     }
 
     BoolVariable& copy(const int& i) {
-        if (i) {state = TRUE; return * this;}
-        state = FALSE;
+        if (i) {state = BoolTRUE; return * this;}
+        state = BoolFALSE;
     }
 
-    enum State {FALSE, TRUE, DC};
+    enum State {BoolFALSE, BoolTRUE, DC};
     State state;
 };
 
@@ -65,25 +65,29 @@ class TruthTable {
 public:
     TruthTable(int inputSize);
     TruthTable(int inputSize, std::vector<int>& v);
-    TruthTable(int inputSize, std::vector<int>&& v);
 
     TruthTable(const TruthTable&) = default;
-    TruthTable(const TruthTable&&) = default;
+    TruthTable(TruthTable&&) = default;
 
     TruthTable& operator=(const TruthTable&) = default;
-    TruthTable& operator=(const TruthTable&&) = default;
+    TruthTable& operator=(TruthTable&&) = default;
 
     const BoolVariable& operator[](int i) const;
     BoolVariable& operator[](int i);
 
     int numInput() {return this->inputSize;}
-    int Size() {return 1 << inputSize;}
+    int size() {return 1 << inputSize;}
+    std::string& getName(int i);
+    void print();
 
 private:
     int inputSize;
+    std::vector<std::string> name;
     std::vector<BoolVariable> data;
 
-    void setByVector(std::vector<int>&& v);
+    void setByVector(std::vector<int>& v);
+    void setNameByVector(std::vector<std::string> &name);
+
 };
 
 
