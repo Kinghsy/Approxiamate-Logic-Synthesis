58acc954f36e81ea8c74035f805e51973552c2c5
tripack45 <patrickyao@sjtu.edu.cn>
2016-12-27 15:38:16 +0800

    Feat. Extract MFFC policy out

---
+++ policy/policy.h
@@ -0,0 +1,21 @@
+//
+// Created by tripack on 16-12-27.
+//
+
+#ifndef VE490_POLICY_H
+#define VE490_POLICY_H
+
+#include "../MFFC/interface.h"
+
+struct FfcSelectPolicy {
+    int minInput;
+    int maxInput;
+
+    FfcSelectPolicy();
+};
+
+BlifBooleanNet::FFC
+selectFFC(const map<BlifBooleanNet::BnetNodeID, BlifBooleanNet::FFC> &ffcCollection,
+          const FfcSelectPolicy &policy = FfcSelectPolicy());
+
+#endif //VE490_POLICY_H
